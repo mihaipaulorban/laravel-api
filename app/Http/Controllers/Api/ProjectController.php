2d7c165ba2e650b782dc7cf17aa93412cef02504
mihaipaulorban <mihaipaulorban@gmail.com>
2024-02-19 21:05:18 +0100

    feat: filtri per api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,20 +10,11 @@ class ProjectController extends Controller
 {
     public function index(Request $request)
     {
-
         $projects = Project::query()
             ->when($request->title, function ($q) use ($request) {
                 return $q->where('title', 'like', "%{$request->title}%");
             })
-            ->when($request->type, function ($q) use ($request) {
-                return $q->where('type_slug', $request->type);
-            })
-            ->when($request->technologies, function ($q) use ($request) {
-                return $q->whereHas('technologies', function ($q) use ($request) {
-                    $q->whereIn('slug', $request->technologies);
-                });
-            })
-            ->get();
+            ->paginate(3);
 
         return $projects;
     }
